b1c411d0278886ef4936905ab9536d40e0a29475
Tim Chenoweth <tchenowe@gmail.com>
2014-09-16 21:55:53 -0600

    First Update

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,33 @@
 
 ## Test to make sure commit and push work.
 
-makeCacheMatrix <- function(x = matrix()) {
+mat<-matrix(c(1,0,1,2,4,0,3,5,6), nrow=3,ncol=3)
 
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv    
 }
